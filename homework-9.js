90ea2e2525618d28b9b21f46951f2ea1de3a5d7d
abdulhalim999 <gulaev.abdul_08@mail.ru>
2026-08-21 20:29:01 +0800

    work with array metods

---
+++ homework-9.js
@@ -0,0 +1,101 @@
+import { comments } from "./comments.js";
+
+//2 задание фильтрация массива чисел
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const newNumbers = numbers.filter(number => number > 4);
+
+console.log(newNumbers);
+
+//3 проверка наличия
+
+const books = ['Война и мир', 'Евгений Онегин', 'Обломов', 'Как читать книги'];
+
+const checking = books.includes('Война и мир');
+
+console.log(checking);
+
+const checking1 = books.includes('Идиот');
+
+console.log(checking1);
+
+//4  перевернуть
+
+function reverseArray(array) {
+    return array.reverse();
+}
+reverseArray(numbers)
+console.log(numbers);
+
+reverseArray(books)
+console.log(books);
+
+//7 комменты
+
+const commentsFilter = comments.filter(comment => comment.email.includes('.com'));
+
+console.log(commentsFilter);
+
+//8 айди
+
+const addPostId = comments.map(comment => {
+    if (comment.id <= 5) {
+        return {
+            ...comment,
+            postId : 2
+        }
+    } else {
+        return {
+            ...comment,
+            postId : 1
+        }
+    }
+})
+console.log(addPostId)
+console.log(comments)
+
+//9 только айди и нэйм
+
+const newComments = comments.map(comment => {
+    return {
+        id : comment.id,
+        name : comment.name,
+    }
+})
+console.log(newComments)
+
+//10 
+
+const addIsInvalid = comments.map(comment => {
+    return {
+        ...comment,
+        isInvalid : comment.body.length > 180,
+    }
+})
+console.log(addIsInvalid)
+
+//11
+
+const arrayOfEmail = comments.reduce((acc, comment) => {
+    acc.push(comment.email)
+    return acc
+},[])
+
+console.log(arrayOfEmail)
+
+const arrayOfEmailWithMap = comments.map(comment => {
+    return comment.email
+})
+
+console.log(arrayOfEmailWithMap)
+
+//12 
+
+const arrayString = arrayOfEmail.toString()
+
+console.log(arrayString)
+
+const arrayJoin = arrayOfEmail.join('-')
+
+console.log(arrayJoin)
